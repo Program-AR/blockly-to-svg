703a4d8caff77df0aade0309003213b7fbfc78a3
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-06-14 11:59:22 -0300

    arrglado espacio del canvas, ahora deberia ser correcto

--- Blockly-to-SVG.js
+++ Blockly-to-SVG.js
@@ -1,21 +1,19 @@
 canvas = Blockly.mainWorkspace.svgBlockCanvas_.cloneNode(true);
-canvas.removeAttribute("width");
-canvas.removeAttribute("height");
 
 if (canvas.children[0] !== undefined) {
     canvas.removeAttribute("transform");
-    canvas.children[0].removeAttribute("transform");
-    canvas.children[0].children[0].removeAttribute("transform");
 
-    var cssContent = Blockly.Css.CONTENT.join('') + '\n\n';
+    var cssContent = Blockly.Css.CONTENT.join('');
+
     var css = '<defs><style type="text/css" xmlns="http://www.w3.org/1999/xhtml"><![CDATA[' + cssContent + ']]></style></defs>';
 
     var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
     var content = new XMLSerializer().serializeToString(canvas);
 
     var xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'
-        + bbox.width + '" height="' + bbox.height + '" viewBox="0 0 ' + bbox.width + ' ' + bbox.height + '">' +
-        css + '"><rect width="100%" height="100%" fill="white"></rect>' + content + '</svg>';
+        + bbox.width + '" height="' + bbox.height + '" viewBox=" ' + bbox.x + ' ' + bbox.y + ' ' + bbox.width + ' ' + bbox.height + '">' +
+        css + '">' + content + '</svg>';
+
 
     let element = document.createElement('a')
     blow = new Blob([xml], { type: 'image/svg+xml;base64' });
