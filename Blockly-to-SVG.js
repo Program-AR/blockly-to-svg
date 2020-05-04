1bcd0bea4505f2294ba74a8cc773fb95ab349ca1
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2020-05-04 16:50:54 -0300

    Adding code to function

--- Blockly-to-SVG.js
+++ Blockly-to-SVG.js
@@ -1,23 +1,27 @@
-canvas = Blockly.mainWorkspace.svgBlockCanvas_.cloneNode(true);
+function exportSVG(){
+    canvas = Blockly.mainWorkspace.svgBlockCanvas_.cloneNode(true);
 
-if (canvas.children[0] !== undefined) {
-    canvas.removeAttribute("transform");
+    if (canvas.children[0] !== undefined) {
+        canvas.removeAttribute("transform");
 
-    var cssContent = Blockly.Css.CONTENT.join('');
+        var cssContent = Blockly.Css.CONTENT.join('');
 
-    var css = '<defs><style type="text/css" xmlns="http://www.w3.org/1999/xhtml"><![CDATA[' + cssContent + ']]></style></defs>';
+        var css = '<defs><style type="text/css" xmlns="http://www.w3.org/1999/xhtml"><![CDATA[' + cssContent + ']]></style></defs>';
 
-    var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
-    var content = new XMLSerializer().serializeToString(canvas);
+        var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
+        var content = new XMLSerializer().serializeToString(canvas);
 
-    var xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'
-        + bbox.width + '" height="' + bbox.height + '" viewBox=" ' + bbox.x + ' ' + bbox.y + ' ' + bbox.width + ' ' + bbox.height + '">' +
-        css + '">' + content + '</svg>';
+        var xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'
+            + bbox.width + '" height="' + bbox.height + '" viewBox=" ' + bbox.x + ' ' + bbox.y + ' ' + bbox.width + ' ' + bbox.height + '">' +
+            css + '">' + content + '</svg>';
 
 
-    let element = document.createElement('a')
-    blow = new Blob([xml], { type: 'image/svg+xml;base64' });
-    element.href = URL.createObjectURL(blow);
-    element.download = 'bloques.svg';
-    element.click();
+        let element = document.createElement('a')
+        blow = new Blob([xml], { type: 'image/svg+xml;base64' });
+        element.href = URL.createObjectURL(blow);
+        element.download = 'bloques.svg';
+        element.click();
+    }
 }
+
+exportSVG()
